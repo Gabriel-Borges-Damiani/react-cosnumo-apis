85d7a1d619bc5d71f1a025280719c1d4ff235c8a
Gabriel Borges Damiani <damiani.gabri70@gmail.com>
2026-05-04 10:41:51 -0300

    Cria logica de editar comentarios e tornar permanente com requisicao PATCH

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,15 +1,31 @@
-import styles from './comment.module.css'
-import { Avatar } from "../Avatar"
-import { ModalComment } from '../ModalComment'
+import styles from "./comment.module.css";
+import { Avatar } from "../Avatar";
+import { ModalComment } from "../ModalComment";
+import { useAuth } from "../../hooks/useAuth";
+import { useState } from "react";
 
 export const Comment = ({ comment }) => {
+  const { user } = useAuth;
+  const [text, setText] = useState(comment.text);
 
+  const isOwner = user && user.id == comment.author.id;
 
-    return (<div className={styles.comment}>
-        <Avatar author={comment.author} />
-        <strong>@{comment.author.name}</strong>
-        <p>{comment.text}</p>
-        <div className={styles.divider} />
-        <ModalComment isEditing />
-    </div>)
-}
+  const handleEdit = (newComment) => setText(newComment.text);
+
+  return (
+    <div className={styles.comment}>
+      <Avatar author={comment.author} />
+      <strong>@{comment.author.name}</strong>
+      <p>{text}</p>
+      <div className={styles.divider} />
+      {isOwner && (
+        <ModalComment
+          isEditing
+          onSuccess={handleEdit}
+          defaultValue={text}
+          commentId={comment.id}
+        />
+      )}
+    </div>
+  );
+};
